da561e1969fab4085dfc7b0b60fbfd87cb4eaefa
Ivan Enzhaev <8observer8@gmail.com>
2023-07-31 04:29:58 +0400

    Replace me.video.CANVAS with me.video.AUTO

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ me.device.onReady(
     () => {
         if (!me.video.init(300, 300, {
                 parent: "screen",
-                renderer: me.video.CANVAS,
+                renderer: me.video.AUTO,
                 scale: "fit",
                 antiAlias: true
             })) //
